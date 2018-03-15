31b51c0aff99c25583617cc4f34a8b53d49f9fc5
Asoskov Aleksander <asoskovai@webinnovations.ru>
2018-03-15 18:03:33 +0300

    commit exercise_01

--- app/src/main/java/ru/icqparty/moneytracker/MainActivity.java
+++ app/src/main/java/ru/icqparty/moneytracker/MainActivity.java
@@ -10,44 +10,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-
-    //onStart, onResume, onPause, onStop, onDestroy,
 
     @Override
     protected void onStart() {
         super.onStart();
-
         Log.i("lifecycle","onStart run");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         Log.i("lifecycle","onResume run");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
         Log.i("lifecycle","onPause run");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
         Log.i("lifecycle","onStop run");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         Log.i("lifecycle","onStart run");
     }
 }
