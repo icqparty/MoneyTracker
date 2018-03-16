823a48158e9c51ccc7c586f142f3000c99bf1ced
Asoskov Alexander <icqparty@gmail.com>
2018-03-16 09:59:10 +0300

    Exercise_02 completed

---
+++ app/src/main/java/ru/icqparty/moneytracker/AddItemActivity.java
@@ -0,0 +1,72 @@
+package ru.icqparty.moneytracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddItemActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "AddItemActivity";
+
+    private EditText nameItem;
+    private EditText valueItem;
+    private Button addButtonItem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additem);
+
+
+        nameItem = findViewById(R.id.name_item);
+        valueItem = findViewById(R.id.value_item);
+        addButtonItem = findViewById(R.id.add_button_item);
+
+
+        TextWatcher watcherChange = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                addButtonItem.setEnabled(false);
+                if (nameItem.getText().length() > 0 && valueItem.getText().length() > 0) {
+                    addButtonItem.setEnabled(true);
+
+                    Log.i(TAG, "afterTextChanged: enable button");
+                }
+            }
+        };
+
+
+        nameItem.addTextChangedListener(watcherChange);
+        valueItem.addTextChangedListener(watcherChange);
+
+
+        addButtonItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = valueItem.getText().toString();
+                String name = nameItem.getText().toString();
+
+                Log.i(TAG, "onClick: add_button_item");
+            }
+        });
+
+
+    }
+}
